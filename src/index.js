5a081f834405e1f0518eef772f38104cf6d816fb
Anton Kuliapin <kuliapin@realtimeboard.com>
2019-01-23 14:01:38 +0500

    fix: modules caching for webpack 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+/*
+  Forked "less-loader" version "4.1.0"
+
+  Why fork?
+  They adapted their plugin for webpack 3+, and removed 'this.cacheable()' call.
+  Seems like webpack 3+ don't need that call.
+  But webpack 1 need that to know, that it can cache modules.
+
+  So the problem is:
+  any changes in watch mode force webpack to recompile all less files, every time!
+
+  Changed lines are marked with comment: // added this line
+ */
+
 import less from 'less';
 import pify from 'pify';
 
@@ -16,6 +30,11 @@ function lessLoader(source) {
     throw new Error(
       'Synchronous compilation is not supported anymore. See https://github.com/webpack-contrib/less-loader/issues/84'
     );
+  }
+
+  // added this line
+  if (this.cacheable) {
+    this.cacheable();
   }
 
   processResult(loaderContext, render(source, options));
